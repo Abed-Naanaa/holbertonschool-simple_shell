d1e63301fa915004b4bb4a5d81922d63c4fbd56c
abed-naanaa <abednaanaa12@gmail.com>
2025-02-25 17:48:23 +0000

    d

--- shell.h
+++ shell.h
@@ -1,15 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
+#define BUFFER_SIZE 1024
 
-extern char **environ;
-
+void prompt(void);
 void execute_command(char *command);
 
-#endif
+#endif /* SHELL_H */
