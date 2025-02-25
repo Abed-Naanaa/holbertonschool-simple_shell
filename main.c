dc7040893a60f69abf43340667866305b043bdc5
abed-naanaa <abednaanaa12@gmail.com>
2025-02-25 18:17:30 +0000

    f

--- main.c
+++ main.c
@@ -1,119 +1,74 @@
 #include "shell.h"
 
-#define MAX_INPUT_SIZE 1024
-
-/**
- * display_prompt - Displays the shell prompt.
- */
-void display_prompt(void)
-{
-    printf("#cisfun$ ");
-    fflush(stdout);
-}
-
-/**
- * trim_whitespace - Trims leading and trailing whitespace from a string.
- * @str: The string to trim.
- *
- * Return: A pointer to the trimmed string.
- */
-char *trim_whitespace(char *str)
-{
-    char *end;
-
-    /* Trim leading space */
-    while (isspace((unsigned char)*str)) str++;
-
-    if (*str == '\0') /* All spaces? */
-        return str;
-
-    /* Trim trailing space */
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end)) end--;
-
-    /* Write new null terminator */
-    *(end + 1) = '\0';
-
-    return str;
-}
-
-/**
- * execute_command - Executes a command using execve.
- * @command: The command to execute.
- */
 void execute_command(char *command)
 {
     pid_t pid;
     int status;
-    char *argv[] = {command, NULL};
+    char *argv[2];
+
+    argv[0] = command;
+    argv[1] = NULL;
 
     pid = fork();
     if (pid == -1)
     {
-        perror("fork");
+        perror("Error: fork failed");
         return;
     }
 
-    if (pid == 0) /* Child process */
+    if (pid == 0)
     {
         if (execve(command, argv, environ) == -1)
         {
-            perror(command);
-            exit(EXIT_FAILURE);
+            perror("Error: execve failed");
+            exit(127);
         }
     }
-    else /* Parent process */
+    else
     {
-        wait(&status);
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            perror("Error: waitpid failed");
+            return;
+        }
+
+        if (WIFEXITED(status))
+        {
+            printf("Child exited with status %d\n", WEXITSTATUS(status));
+        }
+        else if (WIFSIGNALED(status))
+        {
+            printf("Child terminated by signal %d\n", WTERMSIG(status));
+        }
     }
 }
 
-/**
- * main - Entry point of the shell program.
- *
- * Return: Always 0.
- */
 int main(void)
 {
     char *input = NULL;
-    size_t input_size = 0;
-    ssize_t read_size;
-    char *command;
+    size_t len = 0;
+    ssize_t read;
 
     while (1)
     {
-        display_prompt();
-
-        read_size = getline(&input, &input_size, stdin);
-        if (read_size == -1)
+        if (isatty(STDIN_FILENO))
         {
-            /* Handle EOF (Ctrl+D) */
-            printf("\n");
-            free(input);
+            printf("#cisfun$ ");
+            fflush(stdout);
+        }
+
+        read = getline(&input, &len, stdin);
+        if (read == -1)
+        {
+            if (isatty(STDIN_FILENO))
+                printf("\n");
             break;
         }
 
-        /* Remove the newline character from the input */
         input[strcspn(input, "\n")] = '\0';
-
-        /* Split input into commands based on newlines */
-        command = strtok(input, "\n");
-        while (command != NULL)
-        {
-            /* Trim leading/trailing whitespace from the command */
-            command = trim_whitespace(command);
-
-            if (strlen(command) > 0)
-            {
-                /* Execute the command */
-                execute_command(command);
-            }
-
-            /* Get the next command */
-            command = strtok(NULL, "\n");
-        }
+        execute_command(input);
     }
 
-    free(input); /* Free the allocated memory for input */
+    free(input);
     return (0);
 }
